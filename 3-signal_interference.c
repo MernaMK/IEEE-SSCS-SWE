03ffbd3ed1507adcf625d871bcf848af566d937f
MernaMK <mohamedkamalmerna@gmail.com>
2025-04-27 22:31:22 +0300

    1,2,3

---
+++ 3-signal_interference.c
@@ -0,0 +1,20 @@
+# include "stdio.h"
+
+int signal_strength =0;
+void boost_signal(int n)
+{
+int signal_strength =100;
+printf("booster: signal strength = %d",);
+
+}
+
+int main ()
+{
+int strength;
+printf("Enter strength :");
+scanf("%d",&strength);
+if(is_response_detected(strength)==1)
+{printf("Response is detected\n");}
+else
+{printf("Not detected\n");}
+}
