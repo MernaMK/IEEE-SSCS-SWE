3bd18f6a4566ea20124fafb71eb20102f384c71e
MernaMK <mohamedkamalmerna@gmail.com>
2025-04-29 18:56:53 +0300

    3

--- 0x01-session/3-signal_interference.c
+++ 0x01-session/3-signal_interference.c
@@ -1,20 +1,18 @@
-# include "stdio.h"
-
+#include "stdio.h"
 int signal_strength =0;
 void boost_signal(int n)
 {
 int signal_strength =100;
-printf("booster: signal strength = %d",);
+printf("booster: signal strength = %d\n",n);
 
 }
 
 int main ()
 {
-int strength;
+int n;
 printf("Enter strength :");
-scanf("%d",&strength);
-if(is_response_detected(strength)==1)
-{printf("Response is detected\n");}
-else
-{printf("Not detected\n");}
+scanf("%d",&n);
+printf("signal strength = %d\n",signal_strength);
+boost_signal(n);
 }
+
